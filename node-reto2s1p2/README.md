f2d334da815501bb89437efb8abba886ce430c5b
Javier M. A <67672011+javim-cryto@users.noreply.github.com>
2021-10-26 13:27:07 -0500

    Add files via upload

---
+++ node-reto2s1p2/README.md
@@ -0,0 +1,3 @@
+# node-a4m7vw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-a4m7vw)
---
+++ node-reto2s1p2/index.js
@@ -0,0 +1,10 @@
+
+const colors = require('colors');
+
+function suma(numero1, numero2) {
+  let suma = numero1 + numero2;
+  console.log('El resultado de la suma es:'.underline.yellow);
+  console.log(suma);
+}
+
+suma(3, 12);
